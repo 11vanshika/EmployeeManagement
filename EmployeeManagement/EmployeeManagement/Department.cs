d9ea164b6382cd0d1218ccd076b68bf5d075f067
11vanshika <vanshikaa@moback.com>
2022-10-11 19:06:26 +0530

    first commit

---
+++ EmployeeManagement/EmployeeManagement/Department.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    class Department : IDepartment
+    {
+        private int _departmentID;
+        private string _departmentName;
+        public Department()
+        {
+            DepartmentID = 0;
+            DepartmentName = null;
+        }
+        public Department(int departmentId, string departmentName)
+        {
+            DepartmentID = departmentId;
+            DepartmentName = departmentName;
+        }
+
+        public int DepartmentID { get => _departmentID; set => _departmentID = value; }
+        public string DepartmentName { get => _departmentName; set => _departmentName = value; }
+    }
+}
---
+++ EmployeeManagement/EmployeeManagement/IEmployee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeManagement
+{
+    interface IEmployee
+    {
+        int EmployeeDepID { get; set; }
+        int EmployeeID { get; set; }
+        string FullName { get; set; }
+        DateTime DateOfBirth { get; set; }
+        int Experience { get; set; }
+        string Designation { get; set; }
+        int BasicPay { get; set; }
+        int GrossPay { get; set; }
+    }
+}
